628a9001be9cfd4cf8be591d9d62580776d162c6
Cris Bacera <fite_bacera@yahoo.com>
2024-02-06 21:35:58 +0800

    Update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -12,6 +12,7 @@
 
 <script src="{{ URL::asset('/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
+
 <div class="row">
     <div class="col">
         <div class="h-100">
@@ -19,7 +20,7 @@
                 <div class="col-12">
                     <div class="d-flex align-items-lg-center flex-lg-row flex-column">
                         <div class="flex-grow-1">
-                            <h4 class="fs-16 mb-1">Good Morning, {{Auth::user()->name}}</h4>
+                            <h4 class="fs-16 mb-1">Hi, {{Auth::user()->name}}</h4>
                         </div>
                         <div class="mt-3 mt-lg-0">
 
