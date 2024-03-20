40e2c811a987ceeed5cdb8e3635dd453e91d4160
Nilo Besingga <nilo_besingga@aai.com.ph>
2024-03-20 12:32:19 +0800

    update reports

--- app/Exports/ExportRcvDetailed.php
+++ app/Exports/ExportRcvDetailed.php
@@ -19,7 +19,7 @@ class ExportRcvDetailed implements FromCollection, WithHeadings
 
     public function collection()
     {
-        $rcv = RcvHdr::select('rcv_hdr.date_received','rcv_hdr.rcv_no', 'rcv_hdr.po_num', 'p.product_code', 'p.product_name','rd.item_type', 'rd.whse_qty','uw.code as uw_code', 'rd.inv_qty', 'ui.code as ui_code' )
+        $rcv = RcvHdr::select('rcv_hdr.date_received','rcv_hdr.rcv_no', 'rcv_hdr.po_num', 'p.product_code', 'p.product_name','rd.item_type', 'rd.whse_qty','uw.code as uw_code', 'rd.inv_qty', 'ui.code as ui_code' ,'rd.lot_no', 'rd.expiry_date', 'rd.manufacture_date')
             ->leftJoin('rcv_dtl as rd', 'rd.rcv_no', '=', 'rcv_hdr.rcv_no')
             ->leftJoin('products as p', 'p.product_id', '=', 'rd.product_id')
             ->leftJoin('uom as uw', 'uw.uom_id', '=', 'rd.whse_uom')
@@ -71,7 +71,10 @@ class ExportRcvDetailed implements FromCollection, WithHeadings
         'WHSE Qty',
         'UOM',
         'Inv Qty',
-        'UOM'
+        'UOM',
+        'Lot No',
+        'Exp. Date',
+        'Mfg. Date',
        ];
 	}
 }
